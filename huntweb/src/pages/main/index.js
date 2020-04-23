73326e33d104ec7a0e69c719c2699460789364b9
rzamperlini <rzamperlini@gmail.com>
2020-04-22 22:39:03 -0300

    Configurando a navegação - concluído

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import api from '../../services/api';
 import './styles.css';
+import {Link} from 'react-router-dom';
 
 export default class Main extends Component{
 
@@ -56,7 +57,7 @@ export default class Main extends Component{
                     <article key={product._id}>
                        <strong> {product.title}</strong>
                        <p> {product.description}</p>
-                       <a href="">Acessar</a>
+                       <Link to={`/products/${product._id}`}>Acessar</Link>
 
                     </article>
                     ))}
